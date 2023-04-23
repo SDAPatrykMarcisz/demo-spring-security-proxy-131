1acea47a5f8096cd7b6c3ba508538530e3b403df
Patryk <patryk.marcisz.v2@gmail.com>
2023-04-23 11:30:35 +0200

    dodanie wstepnej konfiguracji dla security; wylaczenie mechanizmu csrf tak aby mozna bylo wolac metody typu POST z postmana, dodanie reguly ze kazdy uzytkownik moze wyslac POST na adres /users

--- src/main/java/com/example/demospringsecurityproxy131/config/WebSecurityConfig.java
+++ src/main/java/com/example/demospringsecurityproxy131/config/WebSecurityConfig.java
@@ -1,4 +1,23 @@
 package com.example.demospringsecurityproxy131.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable()); //wylaczamy csrf bo nie korzystamy z frontu, tylko REST-a
+        httpSecurity.authorizeHttpRequests(
+                (request -> request.requestMatchers(HttpMethod.POST, "/users").permitAll())  //kazdy moze wywolac zapytanie dla endpointu /users metoda POST
+        );
+        return httpSecurity.build();
+    }
+
 }
